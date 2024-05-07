355c234930b640b9fa850983cc7041afdd931615
Aprian-Rie <aprythomas5@gmail.com>
2024-05-07 18:41:52 +0300

    Finished the form demo

--- django/basicforms/formsapp/views.py
+++ django/basicforms/formsapp/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
 from . import forms
 
+
 # Create your views here.
 
 def index(request):
     return render(request, 'formsapp/index.html')
 
+
 def form_name_view(request):
     form = forms.FormName()
+
+    if request.method == 'POST':
+        form = forms.FormName(request.POST)
+
+        if form.is_valid():
+            # It worked
+            print("Validation Success")
+            print("Name: " + form.cleaned_data['name'])
+            print("Email: " + form.cleaned_data['email'])
+            print("Text: " + form.cleaned_data['text'])
+
     return render(request, 'formsapp/form_page.html', {'form': form})
